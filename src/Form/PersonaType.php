e0a36f8562e244aab262dc853e540bc60711b385
Jhon Tejada <jhontejada13@gmail.com>
2020-04-25 00:24:37 -0500

    Se crea formulario de creación persona

---
+++ src/Form/PersonaType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Persona;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PersonaType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $persona = new Persona();
+        $builder
+            ->add('nombre', TextType::class)
+            ->add('apellido', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('clave', PasswordType::class)
+            ->add('submit', SubmitType::class, [
+                'label' => 'Registrar' 
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Persona::class,
+        ]);
+    }
+}
