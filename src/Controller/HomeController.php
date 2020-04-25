486d537bd9f7e04c2862f67f8ac71d1e4878774a
Jhon Tejada <jhontejada13@gmail.com>
2020-04-25 00:26:21 -0500

    Se configura método para crear persona

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Persona;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\PersonaType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class HomeController extends AbstractController
 {
@@ -14,8 +19,40 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+        ]); 
+    }
+
+    public function crearPersona(Request $request, UserPasswordEncoderInterface $encoder){
+        $persona = new Persona();//Intancio la clase persona
+
+        $form = $this->createForm(PersonaType::class, $persona);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $persona->getNombre();
+            $persona->getApellido();
+            $persona->getEmail();
+
+            $encoded = $encoder->encodePassword($persona, $persona->getClave());//Cifra la clave con el algoritmo bcrypt añadido en security.yaml
+            $persona->setClave($encoded);//Pasa la clave cifrada
+
+            var_dump($persona);
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($persona);
+            $entityManager->flush();
+
+            $session = new Session();
+            $session->getFlashBag()->add('mensaje', 'Persona creada con exito');
+
+            return $this->redirectToRoute('registro');
+
+        }
+
+        return $this->render('home/crearPersona.html.twig', [
+            'form' => $form->createView()
         ]);
 
-        
     }
 }
