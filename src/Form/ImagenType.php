4dbf1c9d8d4b8f29945d0121c5e8e238b0d32003
Jhon Tejada <jhontejada13@gmail.com>
2020-04-27 23:22:28 -0500

    Se modifica formulario para subir fotos

--- src/Form/ImagenType.php
+++ src/Form/ImagenType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Imagen;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +15,14 @@ class ImagenType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nombre')
-            ->add('descripcion')
-            ->add('foto')
-            ->add('idusuario')
-            ->add('creadoEl')
+            ->add('nombre', TextType::class)
+            ->add('descripcion', TextType::class)
+            ->add('foto', FileType::class, [
+                'label'=> 'Imagen', 
+                'mapped'=> false
+            ])
+            ->add('idusuario', TextType::class)
+            ->add('creadoEl', DateTimeType::class)
         ;
     }
 
